361dbdc113fc38bf20a5b9013447dc6d938c101b
haihs-github <haihs.ptit@gmail.com>
2026-04-08 04:16:04 +0700

    Update feature/sonhai and remove secret keys

---
+++ Code/hotel_booking_app/HotelBackend/index.js
@@ -0,0 +1,37 @@
+// File: index.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// 1. Nhập các "Trưởng phòng" (Routers) vào
+//Sơn Hải
+const hotelRoutes = require('./routes/hotel');
+const bookingRoutes = require('./routes/booking');
+const userRoutes = require('./routes/user');
+
+//Minh
+const adminRoutes = require('./routes/admin');
+
+// 2. Giao việc cho các Trưởng phòng
+// Bất cứ yêu cầu nào bắt đầu bằng '/api/hotels' sẽ được giao cho hotelRoutes xử lý
+//Sơn Hải
+app.use('/api/hotels', hotelRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/users', userRoutes);
+
+//Minh
+app.use('/api/admin', adminRoutes);
+
+// 3. API test thử xem server còn sống không
+app.get('/', (req, res) => {
+	res.send("Xin chào, Backend Node.js đã chạy!");
+});
+
+// 4. Mở cửa lắng nghe
+const PORT = 3000;
+app.listen(PORT, () => {
+	console.log(`🚀 Server đang chạy ngon lành ở port ${PORT}`);
+});
